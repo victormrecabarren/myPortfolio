0e1954edeba713e6b9ca7ca90eb2660c414f91be
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-27 19:24:23 -0400

    added nav bar buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,44 @@ class Header extends Component {
   render(){
     return(
       <>
-      <p>my name</p>
+
+      <p>Victor Recabarren</p>
+      
       <div className="buttonsContainer">
-        <div className="headerButton">About Me</div>
-        <div className="headerButton">portfolio</div>
-        <div className="headerButton">contact me</div>
+        <div
+          onClick={() => {
+            this.props.changeDisplay('about')
+          }}
+          style={this.props.mode== 'about' ? styles.active : null}
+          className="headerButton btn btn-primary">
+          <p style={{marginTop: "-1px"}}>About Me</p>
+        </div>
+        <div
+          onClick={() => {
+            this.props.changeDisplay('portfolio')
+          }}
+          style={this.props.mode== 'portfolio' ? styles.active : null}
+          className="headerButton btn btn-primary">
+          <p style={{marginTop: "-1px"}}>Portfolio</p>
+        </div>
+        <div
+          onClick={() => {
+            this.props.changeDisplay('contact')
+          }}
+          style={this.props.mode== 'contact' ? styles.active : null}
+          className="headerButton btn btn-primary">
+          <p style={{marginTop: "-1px"}}>Contact Me</p>
+        </div>
       </div>
       </>
     )
+  }
+}
+
+const styles = {
+  active: {
+    backgroundColor: "rgba(28,79,127, 1)",
+    color: "white"
   }
 }
 
