43bbacfb689fee87bf8398fe4299583c3063f2ca
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-28 23:11:22 -0400

    Updates

---
+++ precache-manifest.58122cfd8aa445467df7ca6f19932e76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ef7bc614552fab98e6b2e1be50600d",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "b928cb8536d339835d4f",
+    "url": "/myPortfolio/static/css/main.8e19c0cb.chunk.css"
+  },
+  {
+    "revision": "7f79f884645031ff1d11",
+    "url": "/myPortfolio/static/js/2.56f2fe0d.chunk.js"
+  },
+  {
+    "revision": "b928cb8536d339835d4f",
+    "url": "/myPortfolio/static/js/main.5f474f78.chunk.js"
+  },
+  {
+    "revision": "07a9ea0385f9110c1759",
+    "url": "/myPortfolio/static/js/runtime~main.6edf82ad.js"
+  },
+  {
+    "revision": "daac91871a3f153248e92a6911665638",
+    "url": "/myPortfolio/static/media/cupcakes.daac9187.gif"
+  },
+  {
+    "revision": "a34ee705018ee069140a263237c3ff6a",
+    "url": "/myPortfolio/static/media/moon.a34ee705.png"
+  },
+  {
+    "revision": "5ac6d3c618efd56883685ec38d1605b2",
+    "url": "/myPortfolio/static/media/scheduler.5ac6d3c6.gif"
+  },
+  {
+    "revision": "2e51cd0d34516ef44b066f604ef53e1a",
+    "url": "/myPortfolio/static/media/spotify.2e51cd0d.png"
+  },
+  {
+    "revision": "f16e755d50ecbd01d9cc2c0845f1a2b1",
+    "url": "/myPortfolio/static/media/spotify.f16e755d.gif"
+  },
+  {
+    "revision": "27ea5098e5143d0aa0fdc5c390f9790c",
+    "url": "/myPortfolio/static/media/working.27ea5098.jpg"
+  }
+]);
