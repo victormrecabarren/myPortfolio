02dcc30ee5d6b5d1cd10ac2dd9a0f4968febe138
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-28 22:12:22 -0400

    Updates

---
+++ precache-manifest.7d4e71711d280bd11c28ed3643dce6fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9820afb6547f61809f1fd3900c7e2e",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "9769a98ca8517a8a5da8",
+    "url": "/myPortfolio/static/css/main.2a691730.chunk.css"
+  },
+  {
+    "revision": "86273232b9a94d9ea5b6",
+    "url": "/myPortfolio/static/js/2.23af08e9.chunk.js"
+  },
+  {
+    "revision": "9769a98ca8517a8a5da8",
+    "url": "/myPortfolio/static/js/main.b778eef0.chunk.js"
+  },
+  {
+    "revision": "07a9ea0385f9110c1759",
+    "url": "/myPortfolio/static/js/runtime~main.6edf82ad.js"
+  }
+]);
