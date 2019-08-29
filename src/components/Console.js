64d7059030a29cabf0acd7a2f20855bbb640c954
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-28 23:10:58 -0400

    fixing pimage paths

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+// import moon from './assets/images/moon.png'
+import moon from '../assets/moon.png'
+import spotify from '../assets/spotify.png'
+import cupcake from '../assets/cupcake.png'
 
 class Console extends Component {
 
   state = {
-
+    moon: moon,
+    spotify: spotify,
+    cupcake: cupcake
   }
+
 
   render() {
     return(
@@ -20,8 +27,9 @@ class Console extends Component {
 
               <div className={project.name}>
                 <img
-                  src={`/images/${project.icon}`}
-                  alt={project.name}
+                  // src={`../src/assets/images/${project.icon}`}
+                  src={this.state[project.icon]}
+                  alt={project.icon}
                   className="cardIcon"
                  />
               </div>
