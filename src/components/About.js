7c193e7aedfdff53fc1888304bde3b993d4c3a3d
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-30 14:24:18 -0400

    added baltimore skyline img, changing styling of about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import me from '../assets/me.jpg'
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
+import pdf from '../assets/pdf.png'
+import baltimore from '../assets/baltimore.jpg'
+
 
 class About extends Component {
   render(){
     return(
       <div className="aboutComponent">
+
         <div className="aboutMePersonal">
           <div className="headshotBox">
             <img style={{width: "100%"}} src={me} alt="Victor Recabarren" />
@@ -12,12 +18,39 @@ class About extends Component {
           <div className="aboutMeText">
             I am an inquisitive, endlessly curious software engineer motivated by social justice and a desire to put plans into action. I have a passion for learning new information, new approaches, and new frameworks of thinking in order to find deep insights when faced with new problems. I see myself as a lifelong student, and my professional journey through military hospitals, mortgage companies, and law firms have been proxies for me to continuously build my education and add to my problem-solving toolbox.
 
-          ReactJS || JavaScript || NodeJS || Ruby || Git || GitHub || MongoDB || Mongoose || Express || postgreSQL || SQL || HTML5 || CSS ||
+          <p
+            className="skills"
+            >
+              ReactJS || jQuery || JavaScript || NodeJS || Ruby
+              <br/>
+              Rails || REST || Git || GitHub || MongoDB
+              <br/>
+              Mongoose || Express || postgreSQL || SQL || HTML5
+              <br/>
+              || CSS ||
+          </p>
         </div>
       </div>
+
       <div className="aboutMeInfo">
-        linkedin and stuff here
+        <div>
+          <h4>View my LinkedIn</h4>
+          <img className="icons" src={linkedin} />
+        </div>
+        <div>
+          <h4>View my GitHub</h4>
+
+          <img className="icons" src={github} />
+        </div>
+        <div>
+          <h4>Download my Resume</h4>
+
+          <img className="icons" src={pdf} />
+        </div>
+
       </div>
+
+
   </div>
     )
   }
